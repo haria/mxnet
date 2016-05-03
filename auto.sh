ffe40344d8672b6877a4f34edc23d2fbc1d9e70e
haria <guochangzhu@gmail.com>
2016-05-03 11:07:24 +0800

    update auto scipt

--- auto.sh
+++ auto.sh
@@ -3,4 +3,12 @@ git pull upstream
 git merge upstream/master
 git submodule foreach git submodule update
 git push
-
+cd dmlc-core
+git checkout HEAD
+cd -
+cd mshadow
+git checkout HEAD
+cd -
+cd ps-lite
+git checkout HEAD
+cd -
